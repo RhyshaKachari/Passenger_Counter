8c2e220bc3c15ee3f793284963e0dee8f9367016
Rhysha Kachari <rhyshakachari2003@gmail.com>
2023-10-10 15:51:34 +0530

    added increment function

--- passenger_counter/index.js
+++ passenger_counter/index.js
@@ -1,3 +1,9 @@
+let countEl = document.getElementById("count-el")
+console.log(countEl)
+
+let count = 0
+
 function increment(){
-    console.log("The button was clicked")
+    count = count + 1
+    countEl.innerText = count
 }
